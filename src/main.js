292775a34b5ff2d9bba02b895b597d94742d2124
AgathaP <ebrumaktub@gmx.com>
2019-05-17 11:55:30 -0400

    botones para compartir a redes sociales, hago cambios en index.html, data.js, style.css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
+window.onload;
 
+document.getElementById('root').innerHTML = 
+`    
+    <!-- Botones para compartir -->
+<div class="shared-panel">
+<span>Compartir</span><br>
+    <a target="_blank" class="shared fb" title="Facebook"></a>	
+    <a target="_blank" class="shared tt" title="Twitter"></a>
+    <a target="_blank" class="shared gp" title="Google+"></a>
+    <a  data-action='share/whatsapp/share' class="shared wa" title="WhatsApp"></a>
+    <a target="_blank" class="shared ce" id="sml" title="Email"></a>		
+</div>
+`
 
 fetch("https://www.omdbapi.com/?apikey=21b45cac&s=star")
     .then(function(response) {
