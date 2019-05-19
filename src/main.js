537eeef539919b00451a04d14982e1a821aebc00
lau <laura.javiera77@gmail.com>
2019-05-19 12:43:03 -0400

    img fondos mas otra data

--- src/main.js
+++ src/main.js
@@ -1,11 +1,32 @@
 two.innerHTML="";
 let type= "";
 
+fetch("https://api.themoviedb.org/3/movie/120?api_key=67e6ed2c457ae100374fe5478a9f4cc6")
+
+    .then(function(response) {
+        return response.json();
+        console.log(response)
+    })
+   
+    .then(function(data) {
+        data=data
+        console.log(data);
+
+       const forData = data.forEach(element=> {
+                
+        two.innerHTML += `<div> <img class="imagen" src="${element.backdrop_path}"> ${element.title_original} </div>`
+         console.log(element.img);
+   })
+
+ 
+       })
+
+
 document.getElementById("Type").addEventListener("change",()=>{
    type= document.getElementById("bat").text;
    
 
-fetch("https://www.omdbapi.com/?s=batman&apikey=21b45cac")
+fetch("https://www.omdbapi.com/?s=The+Lord+of+the+Rings&type=movie&apikey=21b45cac")
 
     .then(function(response) {
         return response.json();
@@ -24,7 +45,7 @@ fetch("https://www.omdbapi.com/?s=batman&apikey=21b45cac")
     
          })
       
-  
+  /*
 let type2= "";
 document.getElementById("Type").addEventListener("change",()=>{
     type2= document.getElementById("star").text;
@@ -47,5 +68,5 @@ fetch("https://www.omdbapi.com/?s=star&apikey=21b45cac")
    
        })
     
-         })
+         })*/
       
