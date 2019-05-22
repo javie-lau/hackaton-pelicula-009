5c896c316b91ddd9fcb2741321762b714587fbdd
lau <laura.javiera77@gmail.com>
2019-05-22 12:45:03 -0400

    un poquito de cielo despues de la tormenta

--- src/main.js
+++ src/main.js
@@ -29,21 +29,9 @@ document.getElementById('home').addEventListener('click', () => {
 }) 
 
 
-document.getElementById('movies').addEventListener('click', () => {
-  alert('hhhh')
-  document.getElementById('root').innerHTML = 
-  `
-  <select name="" id="movie">
-    <option value="">Películas</option>
-    <option value="&query=Lord+Of+The+rings">El señor de los anillos</option>
-    <option value="&query=hobbit">El hobbit</option>
-  </select>
-  <select name="" id="games">
-    <option value="hobbit&type=game">El hobbit</option>
-    <option value="the-lord-of-the-rings&type=game">El señor de los anillos</option>
-  </select>
-  `
-})
+// document.getElementById('movies').addEventListener('click', () => {
+//   document.getElementById('root').innerHTML = 
+// })
   //llamado a través del select a data tmdb para las peliculas
   document.getElementById('movie').addEventListener('change', () => {  
     document.getElementById('root').innerHTML = ``; 
@@ -69,93 +57,94 @@ document.getElementById('movies').addEventListener('click', () => {
           </div> 
         `
        })
+
     
     // Modal
-  // function modal(theModal){
-  //   theModal.forEach(element =>{
-  //     document.getElementById('movieModalScreen').innerHTML +=
-  //     `
-  //   <!-- Modal -->
-  //   <div id="modal${element.id}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
-  //     <div class="modal-dialog modal-dialog-scrollable" role="document">
-  //       <div class="modal-content">
-  //         <div class="modal-header">
-  //           <h5 class="modal-title" id="exampleModalScrollableTitle">${element.title}</h5>
-  //           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-  //             <span aria-hidden="true">&times;</span>
-  //           </button>
-  //         </div>
-  //         <div class="modal-body">
-  //           <p> ${element.overview} </p>
-  //         </div>
-  //         <div class="modal-footer">
-  //           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-  //           <button type="button" class="btn btn-primary">Save changes</button>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  //     `
-  //   })
-  // }
-  // modal(data);
-   })
+  function modal(theModal){
+    theModal.forEach(element =>{
+      document.getElementById('movieModalScreen').innerHTML +=
+      `
+    <!-- Modal -->
+    <div id="modal${element.id}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalScrollableTitle">${element.title}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p> ${element.overview} </p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+      `
+    })
+  }
+  modal(data);
+})
 })
 
-//select de juegos a data Omdb
-// document.getElementById("others").addEventListener('changes', () => {  
+// select de juegos a data Omdb
+document.getElementById("games").addEventListener('changes', () => {  
 
-// document.getElementById('root').innerHTML = ``;
+document.getElementById('root').innerHTML = ``;
   
-// let gameOrMovie = document.getElementById("others").value;
-// fetch(omdb+gameOrMovie+'&apikey=21b45cac')
-// .then(function(response) {
-//     return response.json();
-// })
-// .then(function(data) {
-//     datas=data.Search;
-//     console.log(datas)
-//     datas.forEach(element=> {
-//       document.getElementById('root').innerHTML += 
-//       `
-//       <div class="poster col-md-4 col-sm-12"> 
-//       <a class="btn" data-toggle="modal" data-target="#modal${element.imdbID}"> 
-//         <img class="image" src="${element.Poster}" alt="${element.Title}"> 
-//       <h5 style="text-align:center"> ${element.Title}<h5>
-//       </div>
-//     `
-//     })
-//         // Modal
-// function modal2(theModal2){
-//   theModal2.forEach(element =>{
-//     document.getElementById('movieModalScreen').innerHTML +=
-//       `
-//       <!-- Modal -->
-//       <div id="modal${element.imdbID}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
-//         <div class="modal-dialog modal-dialog-scrollable" role="document">
-//           <div class="modal-content">
-//             <div class="modal-header">
-//               <h5 class="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
-//               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-//                 <span aria-hidden="true">&times;</span>
-//               </button>
-//             </div>
-//             <div class="modal-body">
-//               <p> ${element.overview} </p>
-//             </div>
-//             <div class="modal-footer">
-//               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-//               <button type="button" class="btn btn-primary">Save changes</button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       `
-//   })
-// }
-// modal2(data);
-// })
-// })
+let gameOrMovie = document.getElementById("games").value;
+fetch(omdb+gameOrMovie+'&apikey=21b45cac')
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    datas=data.Search;
+    console.log(datas)
+    datas.forEach(element=> {
+      document.getElementById('root').innerHTML += 
+      `
+      <div class="poster col-md-4 col-sm-12"> 
+      <a class="btn" data-toggle="modal" data-target="#modal${element.imdbID}"> 
+        <img class="image" src="${element.Poster}" alt="${element.Title}"> 
+      <h5 style="text-align:center"> ${element.Title}<h5>
+      </div>
+    `
+    })
+        // Modal
+function modal2(theModal2){
+  theModal2.forEach(element =>{
+    document.getElementById('movieModalScreen').innerHTML +=
+      `
+      <!-- Modal -->
+      <div id="modal${element.imdbID}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p> ${element.overview} </p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      `
+  })
+}
+modal2(data);
+})
+})
 
 
   
